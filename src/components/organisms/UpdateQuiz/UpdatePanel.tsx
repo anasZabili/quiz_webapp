597d839166a5fc193234e3d2333e9c37be66be5c
David <david.deschamps@etu.univ-rouen.fr>
2022-01-04 19:07:54 +0100

    commit de fin journéé

---
+++ src/components/organisms/UpdateQuiz/UpdatePanel.tsx
@@ -0,0 +1,15 @@
+import { Card, CardContent, Typography } from "@mui/material";
+
+interface UpdateQuizPanelProps {}
+
+const UpdateQuizPanel: React.FC<UpdateQuizPanelProps> = ({}) => {
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography color="primary">COOOOOOL</Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default UpdateQuizPanel;
--- src/components/organisms/UpdateQuiz/index.tsx
+++ src/components/organisms/UpdateQuiz/index.tsx
@@ -1,25 +1,32 @@
-import { IconButton, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
-import DeleteIcon from '@mui/icons-material/Delete';
+import {
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 import QuizList from "./QuizList";
 import useFetchData from "../../../hooks/useFetchData";
 import { useEffect } from "react";
 import { QuizInfoState } from "../../templates/Home";
+import UpdateQuizPanel from "./UpdatePanel";
 
 interface UpdateQuizPageProps {
-    quizzes: QuizInfoState["quiz"];
-    refetch: () => void;
+  quizzes: QuizInfoState["quiz"];
+  refetch: () => void;
 }
 
-const UpdateQuizPage : React.FC<UpdateQuizPageProps> = ({
-    quizzes,
-    refetch
+const UpdateQuizPage: React.FC<UpdateQuizPageProps> = ({
+  quizzes,
+  refetch,
 }) => {
-
-    return (
-        <>
-        <QuizList quizzes={quizzes} refetch={refetch} />
-        </>
-    )
-}
+  return (
+    <>
+      <QuizList quizzes={quizzes} refetch={refetch} />
+      <UpdateQuizPanel />
+    </>
+  );
+};
 
 export default UpdateQuizPage;
